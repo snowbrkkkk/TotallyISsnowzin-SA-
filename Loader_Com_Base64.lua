6328c9e4edb40b5f6ca42195bdc5aefdff325a79
snowbrkkkk <pedro.444.canal.pedro@gmail.com>
2025-05-16 21:18:23 -0300

    Add files via upload

---
+++ Loader_Com_Base64.lua
@@ -0,0 +1,18 @@
+
+local b64 = [[lbmQKCmdhbWU6R2V0U2VydmljZSgiU3RhcnRlckd1aSIpOlNldENvcmUoIlNlbmROb3RpZmljYXRpb24iLCB7IAoJVGl0bGUgPSAiTWFkZSBieSBTbm938J+YruKAjfCfkqggIjsKCVRleHQgPSAiVGhhbmtzIGZvciB1c2luZyBteSBz...]]
+local decode = function(s)
+    local b='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
+    s = string.gsub(s, '[^'..b..'=]', '');
+    return (s:gsub('.', function(x)
+        if x == '=' then return '' end
+        local r,f='',(b:find(x)-1)
+        for i=6,1,-1 do r = r .. (f%2^i - f%2^(i-1) > 0 and '1' or '0') end
+        return r
+    end):gsub('%d%d%d?%d?%d?%d?%d?%d?', function(x)
+        if #x ~= 8 then return '' end
+        local c = 0
+        for i = 1,8 do c = c + (x:sub(i,i)=='1' and 2^(8-i) or 0) end
+        return string.char(c)
+    end))
+end
+loadstring(decode(b64))()
